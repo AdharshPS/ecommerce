2fa5e00d8efab4cfe06155af251d9d11fd2ad4fd
AdharshPS <adharshps000@gmail.com>
2025-10-18 10:32:02 +0530

    Initializing app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainScreen());
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
